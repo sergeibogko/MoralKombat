92a55a77319491c865afb669dc13ba02c65ac5ff
sergeibogko <69677370+sergeibogko@users.noreply.github.com>
2021-06-03 22:00:35 +0300

    corrected-1

--- main.js
+++ main.js
@@ -1,59 +1,54 @@
-const player1 = {
-    name: 'Scorpion',
-    hp: 50,
-    img: src = 'http://reactmarathon-api.herokuapp.com/assets/scorpion.gif',
-    weapon: ['Spear'],
-    attack: function () {
-        console.log(name + '' +'Fight...');
-    }
+const Scorpion = {
+  name: 'Scorpion',
+  hp: 10,
+  img: (src = "http://reactmarathon-api.herokuapp.com/assets/scorpion.gif"),
+  weapon: ['Spear'],
+  attack: function () {
+    console.log('Scorpion' +'Fight...');
+  },
+}
+const Sonya = {
+  name: 'Sonya',
+  hp: 80,
+  img: (srs = "http://reactmarathon-api.herokuapp.com/assets/sonya.gif"),
+  weapon: ['Machete'],
+  attack: function () {
+    console.log('Sonya'+'Fight...');
+  },
 }
 
-const player2 = {
-    name: 'Sonya',
-    hp: 30,
-    img: srs = 'http://reactmarathon-api.herokuapp.com/assets/sonya.gif',
-    weapon: ['Machete'],
-    attack: function () {
-        console.log(name + '' + 'Fight...');
-    }
-}
+const $arenas = document.querySelector(".arenas");
 
-function createPlayer(name, player) {
-    const $player = document.createElement('div');
-    $player.classList.add('player');
+function createPlayer(name, player, life) {
+  const $player = document.createElement("div");
+  $player.classList.add(name);
 
-    const $progressbar = document.createElement('div');
-    $progressbar.classList.add('progressbar');
+  const $progressbar = document.createElement("div");
+  $progressbar.classList.add("progressbar");
 
-    const $life = document.createElement('div');
-    $life.classList.add('life');
-    $life.style.width = '100%';
-    $life.innerHTML = player.hp;
-
-    const $name = document.createElement('div');
-    $name.classList.add('name');
-    $name.innerHTML = player.name;
-    
-    const $character = document.createElement('div');
-    $character.classList.add('character');
-
-    const $img = document.createElement('img');
-    $img.src = player.img;
-
-    const $arenas = document.querySelector('.arenas');
-
+  const $life = document.createElement("div");
+  $life.classList.add("life");
+  $life.style.width = player.hp + '%';
   
+    
 
-   
-    $player.appendChild($progressbar);
-    $progressbar.appendChild($life);
-    $progressbar.appendChild($name);
-    $player.appendChild($character);
-    $character.appendChild($img);
-    $arenas.appendChild($player);
+  const $name = document.createElement("div");
+  $name.classList.add("name");
+  $name.innerHTML = player.name;
 
-    console.log(player);
-}
+  const $character = document.createElement("div");
+  $character.classList.add("character");
 
-createPlayer('player1', player1, 90);
-createPlayer('player2', player2, 70);
+  const $img = document.createElement("img");
+  $img.src = player.img;
+
+  $player.appendChild($progressbar);
+  $progressbar.appendChild($life);
+  $progressbar.appendChild($name);
+  $player.appendChild($character);
+  $character.appendChild($img);
+  $arenas.appendChild($player);
+};
+
+createPlayer("player1", Scorpion,);
+createPlayer("player2", Sonya,);
